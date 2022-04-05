288b6aab68e1d355ef8acf03809d3e85d5afe6ed
zhangxiangxiang <1547084499@qq.com>
2022-04-05 14:59:48 +0800

    生成验证码

--- src/main/java/com/atticus/community/config/AlphaConfig.java
+++ src/main/java/com/atticus/community/config/AlphaConfig.java
@@ -1,16 +1,16 @@
-//package com.atticus.community.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//import java.text.SimpleDateFormat;
-//
-//@Configuration
-//public class AlphaConfig {
-//
-//    @Bean
-//    public SimpleDateFormat simpleDateFormat() {
-//        return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-//    }
-//
-//}
+package com.atticus.community.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.text.SimpleDateFormat;
+
+@Configuration
+public class AlphaConfig {
+
+    @Bean
+    public SimpleDateFormat simpleDateFormat() {
+        return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    }
+
+}
